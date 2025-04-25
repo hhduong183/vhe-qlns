7a5f4046f4c57f7a7dd3f0c6342bce4255341bb8
hhduong183 <hhduong183@gmail.com>
2025-04-25 13:27:10 +0700

    Changes to be committed: 	modified:   App.tsx 	modified:   app/(tabs)/_layout.tsx 	modified:   app/(tabs)/home.tsx Changes not staged for commit: 	modified:   App.tsx 	modified:   app/(tabs)/_layout.tsx 	modified:   app/(tabs)/home.tsx 	modified:   app/(tabs)/index.tsx 	modified:   app/AppNavigator.tsx 	deleted:    app/LoginScreen.tsx 	modified:   app/_layout.tsx 	modified:   app/login.tsx 	modified:   assets/images/icon.png 	modified:   package-lock.json 	modified:   package.json
    	admin.png
    	app/(tabs)/salary.tsx
    	app/(tabs)/settings.tsx
    	app/account-info.tsx
    	app/change-password.tsx
    	app/notifications.tsx
    	assets/default-avatar.png
    	assets/images/icon2.png
    	assets/images/vhe_logo.png

--- App.tsx
+++ App.tsx
@@ -1,5 +1,1 @@
 import { ExpoRoot } from 'expo-router';
-
-export default function App() {
-  return <ExpoRoot />;
-}
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,6 +41,5 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
-
   );
 }
